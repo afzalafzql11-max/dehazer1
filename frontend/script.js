9d7d3d2a68b6519f6bf5b67b00f07b23974e2cd6
afzalafzql11-max <afzalafzql11@gmail.com>
2026-03-11 17:18:04 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,74 +1,123 @@
-function show(id){
+const API="https://image-dehazer-x8aa.onrender.com"
 
-    document.querySelectorAll(".card")
-    .forEach(c=>c.style.display="none")
-    
-    document.getElementById(id).style.display="block"
-    
-    }
-    
-    function signup(){
-    
-    let data=new FormData()
-    
-    data.append("username",sname.value)
-    data.append("email",semail.value)
-    data.append("password",spass.value)
-    
-    fetch("http://127.0.0.1:5000/signup",{
-    method:"POST",
-    body:data
-    })
-    .then(r=>r.json())
-    .then(d=>alert(d.msg))
-    
-    }
-    
-    function login(){
-    
-    let data=new FormData()
-    
-    data.append("email",lemail.value)
-    data.append("password",lpass.value)
-    
-    fetch("http://127.0.0.1:5000/login",{
-    method:"POST",
-    body:data
-    })
-    .then(r=>r.json())
-    .then(d=>alert(d.msg))
-    
-    }
-    
-    function register(){
-    
-    let data=new FormData()
-    
-    data.append("name",name.value)
-    data.append("location",location.value)
-    data.append("email",email.value)
-    data.append("image",photo.files[0])
-    
-    fetch("http://127.0.0.1:5000/register_missing",{
-    method:"POST",
-    body:data
-    })
-    .then(r=>r.json())
-    .then(d=>alert(d.msg))
-    
-    }
-    
-    function find(){
-    
-    let data=new FormData()
-    
-    data.append("image",foundphoto.files[0])
-    
-    fetch("http://127.0.0.1:5000/find",{
-    method:"POST",
-    body:data
-    })
-    .then(r=>r.json())
-    .then(d=>alert(d.msg))
-    
-    }
+let user_id=null
+
+
+async function signup(){
+
+let email=document.getElementById("signup_email").value
+let password=document.getElementById("signup_pass").value
+
+let res=await fetch(API+"/signup",{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+email:email,
+password:password
+})
+
+})
+
+alert("Signup successful")
+
+}
+
+
+
+async function login(){
+
+let email=document.getElementById("login_email").value
+let password=document.getElementById("login_pass").value
+
+let res=await fetch(API+"/login",{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+email:email,
+password:password
+})
+
+})
+
+let data=await res.json()
+
+if(data.status==="success"){
+
+user_id=data.user_id
+
+document.getElementById("auth").style.display="none"
+document.getElementById("dashboard").style.display="block"
+
+}else{
+
+alert("Invalid Login")
+
+}
+
+}
+
+
+
+async function upload(){
+
+let file=document.getElementById("image").files[0]
+
+let form=new FormData()
+
+form.append("user_id",user_id)
+form.append("image",file)
+
+let res=await fetch(API+"/dehaze",{
+
+method:"POST",
+
+body:form
+
+})
+
+let blob=await res.blob()
+
+let url=window.URL.createObjectURL(blob)
+
+let a=document.createElement("a")
+
+a.href=url
+a.download="dehazed.jpg"
+
+a.click()
+
+}
+
+
+
+async function loadHistory(){
+
+let res=await fetch(API+"/history/"+user_id)
+
+let data=await res.json()
+
+let html="<h3>Your Images</h3>"
+
+data.forEach(img=>{
+
+html+=`<br>
+<a href="${API}/download?path=${img}" target="_blank">
+Download Image
+</a>
+`
+
+})
+
+document.getElementById("history").innerHTML=html
+
+}
